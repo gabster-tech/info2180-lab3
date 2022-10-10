92db598d5ec088ff32e3bfbb4e6fd5275af40337
gabster_tech <gabbzbr@outlook.com>
2022-10-09 23:26:20 -0500

    added functionality to add an X or O to a square alternatively when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,6 +4,9 @@
 when the page loads a 3x3 grid game board should be diaplyed*/
 
 window.onload = function(){
+  //empty array to keep track of the state of the game
+  var board = [];
+  var clicks = 9;
   
   //stores a list of all the divs inside a container with the id "board"
   let tiles = document.querySelectorAll("#board div");
@@ -12,5 +15,27 @@ window.onload = function(){
   for(let tile = 0; tile <= 8; tile++){
       tiles[tile].setAttribute("class", "square")
   }
+  
 
+  for(let tile = 0; tile <= 8; tile++){
+      var xo_func = function() {
+          if(clicks%2==1 && tiles[tile].innerHTML==""){
+
+              tiles[tile].setAttribute("class","square O");
+              tiles[tile].innerHTML="O";
+              board[tile] = "O"
+              clicks--;
+          }
+          
+          if(clicks%2==0 && tiles[tile].innerHTML==""){
+
+              tiles[tile].setAttribute("class","square X");
+              tiles[tile].innerHTML="X";
+              board[tile] = "X"
+              clicks--;
+          }
+
+      }
+      tiles[tile].addEventListener('click',xo_func);    
+}
 }
